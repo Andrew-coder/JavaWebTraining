c49db59fdc8b2e93859e3135e10e2f6c9a17774a
Andriy Ivanyuk <Andriy Ivanyuk>
2016-11-02 20:37:05 +0200

    add all parts (reverted from commit 13c13be1404c7faa6e9170a15e8222dcaf3a8331) (reverted from commit 435a04fd12faa0cf5671644e0cd7011d220c3034)

---
+++ Task1/src/main/java/model/GlobalConstants.java
@@ -0,0 +1,15 @@
+package model;
+
+/**
+ * Created by andri on 11/2/2016.
+ */
+public interface GlobalConstants {
+    /**
+     * Default values of minimum border for generating random numbers
+     */
+    int RAND_MIN = 0;
+    /**
+     * Default values of maximum border for generating random numbers
+     */
+    int RAND_MAX = 100;
+}
